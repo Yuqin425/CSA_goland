282a16ef55b5054e05ebf7f7334ea3699773e30e
Yuqin425 <114483458+Yuqin425@users.noreply.github.com>
2022-11-27 18:17:49 +0800

    第三次作业

---
+++ 第三次作业/api/router.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"gin-demo/api/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() {
+	r := gin.Default()
+	r.Use(middleware.CORS())
+
+	r.POST("/register", register)
+	r.POST("/login", login)
+
+	UserRouter := r.Group("/user")
+	{
+		UserRouter.Use(middleware.JWTAuthMiddleware())
+		UserRouter.GET("/get", getUsernameFromToken)
+	}
+
+	r.Run(":8080")
+}
